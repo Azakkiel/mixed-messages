1061d294bbac647051e3af76b7555d9a4c82ffff
Azakkiel <azakkiel@gmail.com>
2021-11-27 12:13:49 +0100

    add functionality, first working version

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let messageComponentsFood = ['goulash ', 'body milk', 'grain'];
-let messageComponentsVerbs = ['eat', 'run', 'despise'];
-let messageComponentsSubst = ['i', 'you', 'we'];
+let messageComponentsFood = ['goulash ', 'body milk ', 'grain '];
+let messageComponentsVerbs = ['eat ', 'love ', 'despise '];
+let messageComponentsSubst = ['i ', 'you ', 'we '];
 const outputMessage = () =>{
-
+    const message = messageComponentsSubst[Math.floor(Math.random()*messageComponentsSubst.length)] + messageComponentsVerbs[Math.floor(Math.random()*messageComponentsVerbs.length)] + messageComponentsFood[Math.floor(Math.random()*messageComponentsFood.length)];
+    console.log(message);
 };
+outputMessage();
