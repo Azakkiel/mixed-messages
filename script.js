807c21fe1142e923bfb1e832a0513fdb8f5094ce
Azakkiel <azakkiel@gmail.com>
2021-11-27 12:07:08 +0100

    declaring message components and function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+let messageComponentsFood = ['goulash ', 'body milk', 'grain'];
+let messageComponentsVerbs = ['eat', 'run', 'despise'];
+let messageComponentsSubst = ['i', 'you', 'we'];
+const outputMessage = () =>{
 
+};
